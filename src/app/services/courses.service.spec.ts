e6b60044a0d23bc1a418318927f4dadfcb600970
manumitsacademy <manumits.info@gmail.com>
2022-02-18 17:34:15 +0530

    added limitless-island api

--- src/app/services/courses.service.spec.ts
+++ src/app/services/courses.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+/* import { TestBed } from '@angular/core/testing';
 
 import { CoursesService } from './courses.service';
 
@@ -14,3 +14,4 @@ describe('CoursesService', () => {
     expect(service).toBeTruthy();
   });
 });
+ */
